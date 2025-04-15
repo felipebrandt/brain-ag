7f4cca89c6d9108ad8e2710d6ce98d9306554332
Felipe Brandt <paulosbrandt@gmail.com>
2025-04-15 14:18:19 -0300

    - Inserido um Card com o Valor da Área Total (ha) - Inserigo um Botão para ligar/desligar Validação CPF/CNPJ - BUGFIX: Pop de farmer no update - Trocado Warning para Error o response quando o produtor não é encontrado
    TODO:
    - Testes Unitários

--- Dockerfile
+++ Dockerfile
@@ -1,19 +1,13 @@
 FROM python:3.9
+ENV PIP_ROOT_USER_ACTION=ignore
 
 WORKDIR /
+EXPOSE 5000
 
 COPY . /
 
-RUN apt-get update && apt-get install -y \
-    build-essential \
-    curl \
-    software-properties-common \
-    git \
-    && rm -rf /var/lib/apt/lists/*
 
 RUN pip install --upgrade pip
-RUN pip3 install -r requirements.txt
+RUN pip install -r requirements.txt
 
-EXPOSE 8501
-
-ENTRYPOINT ["streamlit", "run", "main.py", "--server.port=8501", "--server.address=0.0.0.0"]
+CMD ["python", "main.py", "-h", "0.0.0.0", "-p", "5000"]
--- main.py
+++ main.py
@@ -1,7 +1,26 @@
-import streamlit as st
+from flask import Flask, request, jsonify
+import threading
+
+app = Flask(__name__)
+
+
+def acao_local(dados):
+    print("🚀 Gatilho executado com os dados:", dados)
+
+
+# Endpoint do Webhook
+@app.route('/webhook', methods=['POST', 'GET'])
+def webhook():
+    if request.method == 'POST':
+        dados = request.get_json()
+        print("📥 Dados recebidos no webhook:", dados)
+        acao_local(dados)
+        threading.Thread(target=acao_local, args=(dados,)).start()
+
+        return jsonify({"status": "recebido", "dados": dados}), 200
+    else:
+        return 'oi'
 
 
 if __name__ == '__main__':
-    st.title('Brain Agriculture')
-
-
+    app.run()
